c3288de2efd1a3958b4eaa56351206f81a9ec320
Clayton <cdston@gmail.com>
2022-12-01 20:00:45 -0300

    layout novo tela login

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,46 +4,89 @@
 
 
 @section('conteudo')
-<div class="col-md-4">
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
 
-    @endif
 
-    @if (session('danger'))
-        <div class="alert alert-danger">
-            {{ session('danger') }}
-        </div>
-    @endif
+{{-- Novo --}}
+<section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
+            @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
 
-    <div class="card p-3" >
-        <h3 class="text-center fw-bold text-primary">Faça Login</h3>
-        <div class="card-body">
-            <form method="POST" action="{{route('auth.user')}}">
-            @csrf
-                <div class="mb-3 form-group">
-                  <label for="exampleInputEmail1" class="form-label">Email address</label>
-                  <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            @endif
 
+            @if (session('danger'))
+                <div class="alert alert-danger">
+                    {{ session('danger') }}
                 </div>
-                <div class="mb-3 form-group">
-                  <label for="exampleInputPassword1" class="form-label">Password</label>
-                  <input type="password" name="password" class="form-control" id="exampleInputPassword1">
+            @endif
+
+          <div class="d-flex justify-content-center py-4">
+            <a href="#" class="logo d-flex align-items-center w-auto">
+              <img src="assets/img/logo.png" alt="">
+              <span class="d-none d-lg-block">CastDigital</span>
+            </a>
+          </div><!-- End Logo -->
+
+          <div class="card mb-3">
+
+            <div class="card-body">
+
+              <div class="pt-4 pb-2">
+                <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
+                <p class="text-center small">Enter your username & password to login</p>
+              </div>
+
+              <form method="POST" action="{{route('auth.user')}}" class="row g-3 needs-validation" novalidate>
+                @csrf
+                <div class="col-12">
+                  <label for="yourUsername" class="form-label">Username</label>
+                  <div class="input-group has-validation">
+                    <span class="input-group-text" id="inputGroupPrepend">@</span>
+                    <input type="email" name="email" class="form-control" id="yourUsername" required>
+                    <div class="invalid-feedback">Please enter your username.</div>
+                  </div>
                 </div>
 
-                <button type="submit" class="btn btn-primary">Submit</button>
+                <div class="col-12">
+                  <label for="yourPassword" class="form-label">Password</label>
+                  <input type="password" name="password" class="form-control" id="yourPassword" required>
+                  <div class="invalid-feedback">Please enter your password!</div>
+                </div>
+
+                {{-- <div class="col-12">
+                  <div class="form-check">
+                    <input class="form-check-input" type="checkbox" name="remember" value="true" id="rememberMe">
+                    <label class="form-check-label" for="rememberMe">Remember me</label>
+                  </div>
+                </div> --}}
+                <div class="col-12">
+                  <button class="btn btn-primary w-100" type="submit">Login</button>
+                </div>
+                <div class="col-12">
+                  <p class="small mb-0">Don't have account? <a href="#">Create an account</a></p>
+                </div>
               </form>
+
+            </div>
+          </div>
+
+          <div class="credits">
+
+            Designed by <a href="#">CastDigital</a>
+          </div>
+
         </div>
+      </div>
     </div>
 
+  </section>
 
-
-
-</div>
 @endsection
--- resources/views/login/main.blade.php
+++ resources/views/login/main.blade.php
@@ -1,19 +1,46 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="pt-br">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-    <title>@yield('title')</title>
-  </head>
-  <body>
+  <title>@yield('title')</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
 
-    <div class="container-fluid min-vh-100 d-flex align-items-center bg-dark ">
-        <div class="row w-100 d-flex justify-content-center">
+  <!-- Favicons -->
+  <link href="{{asset('assets/img/favicon.png')}}" rel="icon">
+  <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.gstatic.com" rel="preconnect">
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/quill/quill.snow.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/quill/quill.bubble.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/remixicon/remixicon.css')}}" rel="stylesheet">
+  <link href="{{asset('assets/vendor/simple-datatables/style.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('assets/css/style.css')}}" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: NiceAdmin - v2.4.1
+  * Template URL: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+    <main>
+        <div class="container">
 
             @yield('conteudo')
         </div>
@@ -21,15 +48,23 @@
     </div>
 
 
-    <!-- Optional JavaScript; choose one of the two! -->
+</main><!-- End #main -->
 
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
-  </body>
+<!-- Vendor JS Files -->
+<script src="{{asset('assets/vendor/apexcharts/apexcharts.min.js')}}"></script>
+<script src="{{asset('assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<script src="{{asset('assets/vendor/chart.js/chart.min.js')}}"></script>
+<script src="{{asset('assets/vendor/echarts/echarts.min.js')}}"></script>
+<script src="{{asset('assets/vendor/quill/quill.min.js')}}"></script>
+<script src="{{asset('assets/vendor/simple-datatables/simple-datatables.js')}}"></script>
+<script src="{{asset('assets/vendor/tinymce/tinymce.min.js')}}"></script>
+<script src="{{asset('assets/vendor/php-email-form/validate.js')}}"></script>
+
+<!-- Template Main JS File -->
+<script src="{{asset('assets/js/main.js')}}"></script>
+
+</body>
+
 </html>
