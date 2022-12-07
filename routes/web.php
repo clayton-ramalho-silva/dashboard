dbf6804d93cdaf245c615b9cbc854acef18817b7
Clayton <cdston@gmail.com>
2022-12-07 13:03:12 -0300

    links no menu perfil

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,7 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('admin')->group(function(){
 
     Route::get('/', [AdminController::class, 'index'])->name('admin.index');
+    Route::get('/users-profile',[AdminController::class, 'users_profile'])->name('admin.users_profile');
 });
 
 
@@ -34,6 +35,12 @@ Route::redirect('/', '/login');
 Route::get('/login', [LoginController::class, 'login'])->name('login.page');
 Route::post('/auth',[LoginController::class, 'auth'])->name('auth.user');
 Route::post('/logout', [LoginController::class,'logout'])->name('logout');
+Route::get('/register', [LoginController::class, 'register'])->name('register.page');
+Route::post('/register', [LoginController::class, 'create'])->name('create.acount');
+
+
+
+
 
 Route::get('teste', function () {
     return view('welcome');
